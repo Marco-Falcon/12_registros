45dd24ee52252de6a4a0481970ae114cb1af95a1
Marco-Falcon <mfalconl@unjbg.edu.pe>
2026-07-01 10:12:35 -0500

    Agregar archivo empleados.cpp con código avanzado

---
+++ empleados.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//
+struct empleados{
+	string numero;
+	string nombres;
+	int ventas[12];
+	float salario;
+};
+
+int main(){
+	int n;
+	empleados lista[150];
+	
+	cout<<"ingrese la acntidad de empleados: ";
+	cin>>n;
+	
+}
